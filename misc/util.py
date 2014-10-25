b5df036634b603d8cf0070c35ea3c64978929cd0
samfway <samfway@gmail.com>
2014-10-25 11:59:06 -0600

    Added helper functions

--- misc/util.py
+++ misc/util.py
@@ -9,5 +9,31 @@ __status__ = "Development"
 
 
 class Struct:
+    """ Create a Python object from a dictionary of key-values """ 
     def __init__(self, **entries): 
         self.__dict__.update(entries)
+
+
+def custom_cast(x, cast_types=[int, float, str]):
+    """ Attempt to cast x using the specified types in the order
+        in which they appear.  """ 
+    for cast_func in cast_types:
+        try:
+            return cast_func(x)
+        except ValueError:
+            pass
+    raise BaseException('All casts failed!')
+
+
+def add_weighted_edge(G, vertex_pair, weight=1.0):
+    """ Wrapper to facilitate making weighted, directed
+        networks.  If an edge exists between the vertex_pair,   
+        the weight is incremented by the supplied value. 
+        If the edge does not already exist, it is created 
+        and initialized with the supplied weight value.
+    """ 
+    s, d = vertex_pair  # unpack
+    if G.has_edge(s, d):
+        G[s][d]['weight'] += weight
+    else:
+        G.add_edge(s,d, weight=weight)
